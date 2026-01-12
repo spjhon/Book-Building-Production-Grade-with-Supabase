ff4a0bef35b0131e8f8fa4a3b15b8fe90173afe9
PC Escritorio <spjhon@gmail.com>
2026-01-11 20:25:30 -0500

    se termina auth pero sin el formulario de sing up

--- nextsupabase/src/lib/utils.ts
+++ nextsupabase/src/lib/utils.ts
@@ -1,6 +1,17 @@
+import { AuthError } from "@supabase/supabase-js"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function mapSupabaseAuthError(error: string) {
+  switch (error) {
+    case "AuthSessionMissingError":
+      return "No hay datos de usuario"
+
+    default:
+      return "No se pudo completar la operación"
+  }
 }
